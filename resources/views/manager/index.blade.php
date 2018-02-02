35346a7554cabcbf7e6d59e0151d55c723d85ba5
DESKTOP-8MS0SJG\DCM <duccm.vitracao@gmail.com>
2018-02-02 19:42:32 +0700

    cập nhật giao diện

--- resources/views/manager/index.blade.php
+++ resources/views/manager/index.blade.php
@@ -4,58 +4,82 @@
 
 <div class="row">
     <div class="col-md-12">
-        <!-- BEGIN BORDERED TABLE PORTLET-->
-        <div class="portlet light portlet-fit bordered">
+        <!-- BEGIN EXAMPLE TABLE PORTLET-->
+        <div class="portlet light bordered">
             <div class="portlet-title">
-                <div class="caption">
-                    <i class="icon-bubble font-dark"></i>
-                    <span class="caption-subject font-dark bold uppercase">Danh sách Job</span>
+                <div class="caption font-dark">
+                    <i class="icon-settings font-dark"></i>
+                    <span class="caption-subject bold uppercase"> Danh sách job</span>
                 </div>
-                <div class="actions">
-                    <div class="btn-group">
-                        <a class="btn dark btn-outline btn-circle btn-sm" href="{{route('manager-create-job')}}" >Tạo Job</a>
-                    </div>
-                </div>                
             </div>
             <div class="portlet-body">
-                <div class="table-scrollable">
-                    <table class="table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Tên Job</th>
-                                <th>Số lượng</th>
-                                <th>Order</td>
-                                <th>Trạng thái</th>
-                                <th>Thao tác</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($jobs as $job)
-                            <tr>
-                                <td>{{$job->id}}</td>
-                                <td>{{$job->name}}</td>
-                                <td>{{$job->total}}</td>
-                                <td>
-                                @if(isset($job->order->id))
-                                    {{count($job->order)}}
-                                @else
-                                    0
-                                @endif
-                                </td>
-                                <td> <span class="label label-sm label-info">{{$job->status->status}}</span> </td>
-                                <td> 
-                                    <a href="{{route('manager-show-job',$job->id)}}" class="btn btn-outline btn-circle dark btn-sm black">Xem chi tiết</a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                <div class="table-toolbar">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="btn-group">
+                                <a id="sample_editable_1_new" class="btn sbold green" href="{{route('manager-create-job')}}"> Tạo Job
+                                    <i class="fa fa-plus"></i>
+                                </a >
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
+                <table class="table table-striped table-bordered table-hover table-checkable order-column" id="sample_1">
+                    <thead>
+                        <tr>
+                            <th> Id </th>
+                            <th> Tên Job </th>
+                            <th> Số lượng </th>
+                            <th> Order </th>
+                            <th> Trạng thái</th>
+                            <th> Thao tác </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if(isset($jobs))
+                        @foreach($jobs as $job)
+                        <tr class="odd gradeX">
+                            <td> {{$job->id}} </td>
+                            <td>
+                                {{$job->name}}
+                            </td>
+                            <td>
+                                {{$job->total}}
+                            </td>
+                            <td>
+                                {{count($job->order)}}
+                            </td>
+                            <td>
+                                {{$job->status->status}}
+                            </td>
+                            <td>
+                                <div class="btn-group">
+                                    <button class="btn btn-xs green dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false"> Thao tác
+                                        <i class="fa fa-angle-down"></i>
+                                    </button>
+                                    <ul class="dropdown-menu pull-left" role="menu">
+                                        <li>
+                                            <a href="{{route('manager-show-job',$job->id)}}"> Chi tiết </a>
+                                        </li>
+                                        <li>
+                                            <a href="{{route('manager-edit-job',$job->id)}}"> Sửa </a>
+                                        </li>
+                                        <li>
+                                            <a href="{{route('manager-delete-job',$job->id)}}"> Xóa </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                        @endif
+                    </tbody>
+                </table>
             </div>
         </div>
-        <!-- END BORDERED TABLE PORTLET-->
+        <!-- END EXAMPLE TABLE PORTLET-->
     </div>
-</div> 
+</div>
 
 @stop
--- resources/views/qa/layout/master.blade.php
+++ resources/views/qa/layout/master.blade.php
@@ -18,10 +18,18 @@
     <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
     <!-- END GLOBAL MANDATORY STYLES -->
     <!-- BEGIN PAGE LEVEL PLUGINS -->
+    @if(request()->route()->getName() != 'qa-index' || request()->route()->getName() != 'qa-checklist')
     <link href="../assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.css" rel="stylesheet" type="text/css" />
     <link href="../assets/global/plugins/morris/morris.css" rel="stylesheet" type="text/css" />
     <link href="../assets/global/plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet" type="text/css" />
     <link href="../assets/global/plugins/jqvmap/jqvmap/jqvmap.css" rel="stylesheet" type="text/css" />
+
+    <link href="../assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+    @else
+    <link href="../assets/global/plugins/datatables/datatables.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css" rel="stylesheet" type="text/css">
+    @endif
     <!-- END PAGE LEVEL PLUGINS -->
     <!-- BEGIN THEME GLOBAL STYLES -->
     <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
@@ -90,6 +98,7 @@
     <script src="../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
     <!-- END CORE PLUGINS -->
     <!-- BEGIN PAGE LEVEL PLUGINS -->
+    @if(request()->route()->getName() != 'qa-index' || request()->route()->getName() != 'qa-checklist')
     <script src="../assets/global/plugins/moment.min.js" type="text/javascript"></script>
     <script src="../assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js" type="text/javascript"></script>
     <script src="../assets/global/plugins/morris/morris.min.js" type="text/javascript"></script>
@@ -120,12 +129,25 @@
     <script src="../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js" type="text/javascript"></script>
     <script src="../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js" type="text/javascript"></script>
     <script src="../assets/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js" type="text/javascript"></script>
+
+    <script src="../assets/global/plugins/select2/js/select2.full.min.js" type="text/javascript"></script>
+    @else
+    <script src="../assets/global/scripts/datatable.js" type="text/javascript"></script>
+    <script src="../assets/global/plugins/datatables/datatables.min.js" type="text/javascript"></script>
+    <script src="../assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js" type="text/javascript"></script>
+    @endif
     <!-- END PAGE LEVEL PLUGINS -->
     <!-- BEGIN THEME GLOBAL SCRIPTS -->
     <script src="../assets/global/scripts/app.min.js" type="text/javascript"></script>
     <!-- END THEME GLOBAL SCRIPTS -->
     <!-- BEGIN PAGE LEVEL SCRIPTS -->
+    @if(request()->route()->getName() != 'qa-index' || request()->route()->getName() != 'qa-checklist')
     <script src="../assets/pages/scripts/dashboard.min.js" type="text/javascript"></script>
+
+    <script src="../assets/pages/scripts/components-select2.min.js" type="text/javascript"></script>
+    @else
+    <script src="../assets/pages/scripts/table-datatables-managed.min.js" type="text/javascript"></script>
+    @endif
     <!-- END PAGE LEVEL SCRIPTS -->
     <!-- BEGIN THEME LAYOUT SCRIPTS -->
     <script src="../assets/layouts/layout/scripts/layout.min.js" type="text/javascript"></script>
